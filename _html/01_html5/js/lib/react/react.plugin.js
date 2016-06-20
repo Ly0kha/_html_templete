6f65b540d9bc8e0e02c2e8873055847b159ee6b6
Atsushi Kikushima <atsushi.kikushima@gmail.com>
2016-06-20 20:34:50 +0900

    20160620 React Learning
    Propsの使い方を詰め込んでみる
    Proptypesでのrequiredが確認出来てないのでもう少し煮詰めたい

--- _html/01_html5/js/lib/react/react.plugin.js
+++ _html/01_html5/js/lib/react/react.plugin.js
@@ -23,12 +23,10 @@ var kerningJsonFirstDir = ('ajax/kerning.json');
 $.getJSON(kerningJsonFirstDir, function(data) {
 
 
-	// var React = require('react');
-	// var ReactDOM = require('react-dom');
-
-
 	/*【TEST】 React実装
 	------------------------------------------------------------*/
+
+
 	var Hello = React.createClass({
 		displayName: 'Hello',
 		render: function() {
@@ -36,11 +34,13 @@ $.getJSON(kerningJsonFirstDir, function(data) {
 		}
 	});
 
-
 	ReactDOM.render(
 		React.createElement(Hello, {name: 'World'}),
 		document.getElementById('test06')
 	);
+
+
+	/*----------------------------------------------------------*/
 
 
 	var Test = React.createClass({
@@ -50,11 +50,13 @@ $.getJSON(kerningJsonFirstDir, function(data) {
 		}
 	});
 
-
 	ReactDOM.render(
 		React.createElement(Test, {name: 'Rolling Thunder'}),
 		document.getElementById('test07')
 	);
+
+
+	/*----------------------------------------------------------*/
 
 
 	var HelloWorld = React.createClass({
@@ -68,15 +70,24 @@ $.getJSON(kerningJsonFirstDir, function(data) {
 	var m = ReactDOM.render(<HelloWorld />, document.getElementById('test08'));
 
 
+	/*----------------------------------------------------------*/
+
+
 	var Hirata = React.createClass({
 		render: function() {
 				return (
-					<p className='box mb-20'>お前、{this.props.name}</p>
+					<div>
+						<p className='box'>お前、{this.props.name}</p>
+						<p className='box mb-20'>しょっぱいコーディングですみません！</p>
+					</div>
 				);
 		}
 	});
 
-	var m = ReactDOM.render(<Hirata name='平田だろ？' />, document.getElementById('test09'));
+	var m = ReactDOM.render(<Hirata name='平田だろ!?' />, document.getElementById('test09'));
+
+
+	/*----------------------------------------------------------*/
 
 
 	var MyComponent = React.createClass({
@@ -87,36 +98,150 @@ $.getJSON(kerningJsonFirstDir, function(data) {
 		}
 	});
 
-	var m = ReactDOM.render(<MyComponent name='名無しだろ？' />, document.getElementById('test10'));
+	var m = ReactDOM.render(<MyComponent name='名無しだろ!?' />, document.getElementById('test10'));
 
 
-	React.createClass({
-		displayName : '',
-		propTypes: {},
-		mixins : [],
-		getInitialState : function() {},
-		componentWillMount : function() {},
-		componentWillUnmount : function() {},
-		_onChange : function() {},
-		_onCreate : function() {},
-		render : function() {}
+	/*----------------------------------------------------------*/
+
+
+	var Counter = React.createClass ({
+
+		getInitialState() {
+			return {
+				count: 0
+			};
+		},
+
+		onClick() {
+			this.setState({count: this.state.count + 1 });
+		},
+
+		render () {
+			return (
+				<div>
+					<div>
+						count: {this.state.count}
+					</div>
+					<button onClick={this.onClick}>
+						click!
+					</button>
+				</div>
+			);
+		}
+
 	});
+
+	var m = ReactDOM.render(<Counter />, document.getElementById('test11'));
+
+
+	/*----------------------------------------------------------*/
+
+
+	var Avatar = React.createClass ({
+
+		render() {
+			var avatarImg = `images/avatar/avatar_${this.props.user.id}.png`;
+			return (
+				<div className='box center mb-10'>
+					<img src={avatarImg} className='wd-10' />
+					<p className='small mg-00 center'>
+						{this.props.user.name}
+					</p>
+				</div>
+			);
+		}
+
+	});
+
+	var user = {
+		id: 10,
+		name: 'Atsushi Kikushima'
+	};
+
+	var m = ReactDOM.render(<Avatar user={user} />, document.getElementById('test12'));
+
+
+	/*----------------------------------------------------------*/
+
+
+	/*----------------------------------------------------------
+
+	React.PropTypes.array           // 配列
+	React.PropTypes.bool.isRequired // Booleanで必須
+	React.PropTypes.func            // 関数
+	React.PropTypes.number          // 数値
+	React.PropTypes.object          // オブジェクト
+	React.PropTypes.string          // 文字列
+	React.PropTypes.node            // Renderできるもの
+	React.PropTypes.element         // React Element
+	React.PropTypes.instanceOf(XXX) // XXXのinstanceかどうか
+	React.PropTypes.oneOf(['foo', 'bar']) // fooかbar
+	React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.array]) // 文字列か配列
+	React.PropTypes.arrayOf(React.PropTypes.string)  // 文字列の配列かどうか
+	React.PropTypes.objectOf(React.PropTypes.string) // 文字列の値を持っているか
+	React.PropTypes.shape({                          // 指定された形式を満たしているかどうか
+		color: React.PropTypes.string,
+		fontSize: React.PropTypes.number
+	});
+	React.PropTypes.any.isRequired  // なんでもいいけど必須
+
+	----------------------------------------------------------*/
+
+
+	var Avatar02 = React.createClass({
+
+		propTypes: {
+			user: React.PropTypes.shape({
+				name:   React.PropTypes.string.isRequired,
+				id:     React.PropTypes.number.isRequired,
+				width:  React.PropTypes.number.isRequired,
+				height: React.PropTypes.number.isRequired,
+				alt:    React.PropTypes.string
+			})
+		},
+
+		render() {
+			var src = `images/avatar/avatar_${this.props.user.id}.png`;
+			return (
+
+				<div className='box center mb-10'>
+					<img src={src} width={this.props.user.width} height={this.props.user.height} alt={this.props.user.alt} />
+					<p className='small mg-00 center'>
+						{this.props.user.name}
+					</p>
+				</div>
+
+			);
+
+		}
+
+	});
+
+	var user = {
+		name: 'Atsushi Kikushima',
+		id: 10,
+		width: 100,
+		height: 100,
+		alt: 'ヒマ人'
+	};
+
+	var m = ReactDOM.render(<Avatar02 user={user} />, document.getElementById('test13'));
 
 
 	/*【TEST】 React実装 ここまで
 	------------------------------------------------------------*/
 
 
+	setTimeout(function() {
 
+		/* 中にテキストが入るタグの判定 */
+		var tag = $('p, h1, h2, h3, h4, h5, h6, .carousel-caption, dl.news dt, dl.news dd, dl#column dt, dl#column dd, dl#form-layout-jp dt, dl#form-layout-en dt, ul.list li, ol.list li, ul.suggest-menu li, ul.form-accept li, th, td, a, address')
 
-	/* 中にテキストが入るタグの判定 */
-	var tag = $('p, h1, h2, h3, h4, h5, h6, .carousel-caption, dl.news dt, dl.news dd, dl#column dt, dl#column dd, dl#form-layout-jp dt, dl#form-layout-en dt, ul.list li, ol.list li, ul.suggest-menu li, ul.form-accept li, th, td, a, address')
+		$(tag).kerning({
+			'data': data
+		});
 
-	$(tag).kerning({
-		'data': data
-	});
-
-
+	}, 1000);
 
 
 });
